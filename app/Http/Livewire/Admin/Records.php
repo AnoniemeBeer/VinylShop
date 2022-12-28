08729bfaf93fdfd72535377ea161bb88052217f5
AnoniemeBeer <jorn.van.dijck@gmail.com>
2022-12-28 23:58:31 +0100

    admin: records completed up to exercises

--- app/Http/Livewire/Admin/Records.php
+++ app/Http/Livewire/Admin/Records.php
@@ -4,8 +4,12 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 use Livewire\WithPagination;
 use App\Models\Record;
+use App\Models\Genre;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class Records extends Component
 {
@@ -29,39 +33,135 @@ class Records extends Component
         'cover' => '/storage/covers/no-cover.png',
     ];
 
+    // validation attributes
+    protected $validationAttributes = [
+        'newRecord.mb_id' => 'MusicBrainz id',
+        'newRecord.artist' => 'artist name',
+        'newRecord.title' => 'record title',
+        'newRecord.genre_id' => 'genre',
+        'newRecord.stock' => 'stock',
+        'newRecord.price' => 'price',
+    ];
+
+    public $genres;
+
     // validation rules (use the rules() method, not the $rules property)
     protected function rules()
     {
-        return [];
+        return
+            [
+                'newRecord.mb_id' => 'required|size:36|unique:records,mb_id,' . $this->newRecord['id'],
+                'newRecord.artist' => 'required',
+                'newRecord.title' => 'required',
+                'newRecord.genre_id' => 'required|exists:genres,id',
+                'newRecord.stock' => 'required|numeric|min:0',
+                'newRecord.price' => 'required|numeric|min:0',
+            ];
     }
 
-    // validation attributes
-    protected $validationAttributes = [];
+    // get artist, title and cover from the MusicBrainz API
+    public function getDataFromMusicbrainzApi()
+    {
+        $this->validateOnly('newRecord.mb_id');
+        $this->resetErrorBag();
+        $response = Http::get('https://musicbrainz.org/ws/2/release/' . $this->newRecord['mb_id'] . '?inc=artists&fmt=json');
+        if ($response->successful()) {
+            $data = $response->json();
+            $this->newRecord['artist'] = $data['artist-credit'][0]['artist']['name'];
+            $this->newRecord['title'] = $data['title'];
+            if ($data['cover-art-archive']['front']) {
+                $this->newRecord['cover'] = 'https://coverartarchive.org/release/' . $this->newRecord['mb_id'] . '/front-250.jpg';
+                $originalCover = Image::make($this->newRecord['cover'])->encode('jpg', 75);
+                Storage::disk('public')->put('covers/' . $this->newRecord['mb_id'] . '.jpg', $originalCover);
+            }
+        } else {
+            $this->newRecord['artist'] = null;
+            $this->newRecord['title'] = null;
+            $this->newRecord['cover'] = '/storage/covers/no-cover.png';
+            $this->addError('newRecord.mb_id', 'MusicBrainz id not found');
+        }
+    }
 
     // set/reset $newRecord and validation
-    public function setNewRecord()
+    public function setNewRecord(Record $record = null)
     {
+        $this->resetErrorBag();
+        if ($record) {
+            $this->newRecord['id'] = $record->id;
+            $this->newRecord['artist'] = $record->artist;
+            $this->newRecord['title'] = $record->title;
+            $this->newRecord['mb_id'] = $record->mb_id;
+            $this->newRecord['stock'] = $record->stock;
+            $this->newRecord['price'] = $record->price;
+            $this->newRecord['genre_id'] = $record->genre_id;
+            $this->newRecord['cover'] =
+                Storage::disk('public')->exists('covers/' . $record->mb_id . '.jpg')
+                ? '/storage/covers/' . $record->mb_id . '.jpg'
+                : '/storage/covers/no-cover.png';
+        } else {
+            $this->reset('newRecord');
+        }
+        $this->showModal = true;
     }
 
     // reset the paginator
     public function updated($propertyName, $propertyValue)
     {
-        $this->resetPage();
+        if (in_array($propertyName, ['search', 'noStock', 'noCover', 'perPage']))
+            $this->resetPage();
     }
 
     // create a new record
     public function createRecord()
     {
+        $this->validate();
+        $record = Record::create([
+            'mb_id' => $this->newRecord['mb_id'],
+            'artist' => $this->newRecord['artist'],
+            'title' => $this->newRecord['title'],
+            'stock' => $this->newRecord['stock'],
+            'price' => $this->newRecord['price'],
+            'genre_id' => $this->newRecord['genre_id'],
+        ]);
+        $this->showModal = false;
+        $this->dispatchBrowserEvent('swal:toast', [
+            'background' => 'success',
+            'html' => "The record <b><i>{$record->title} from {$record->artist}</i></b> has been added",
+        ]);
     }
 
     // update an existing record
-    public function updateRecord()
+    public function updateRecord(Record $record)
     {
+        $this->validate();
+        $record->update([
+            'mb_id' => $this->newRecord['mb_id'],
+            'artist' => $this->newRecord['artist'],
+            'title' => $this->newRecord['title'],
+            'stock' => $this->newRecord['stock'],
+            'price' => $this->newRecord['price'],
+            'genre_id' => $this->newRecord['genre_id'],
+        ]);
+        $this->showModal = false;
+        $this->dispatchBrowserEvent('swal:toast', [
+            'background' => 'success',
+            'html' => "The record <b><i>{$record->title} from {$record->artist}</i></b> has been updated",
+        ]);
     }
 
     // delete an existing record
-    public function deleteRecord()
+    public function deleteRecord(Record $record)
     {
+        $record->delete();
+        $this->dispatchBrowserEvent('swal:toast', [
+            'background' => 'success',
+            'html' => "The record <b><i>{$record->title} from {$record->artist}</i></b> has been deleted",
+        ]);
+    }
+
+    public function mount()
+    {
+        $this->genres = Genre::orderBy('name')->get();
     }
 
     public function render()
@@ -72,6 +172,9 @@ class Records extends Component
         // only if $noCover is true, filter the query further, else, skip this step
         if ($this->noStock)
             $query->where('stock', false);
+        // only if $noCover is true, filter the query further. else, skip this step
+        if ($this->noCover)
+            $query->coverExists(false);
         // paginate the $query
         $records = $query->paginate($this->perPage);
         return view('livewire.admin.records', compact('records'))
--- resources/views/livewire/admin/records.blade.php
+++ resources/views/livewire/admin/records.blade.php
@@ -7,8 +7,8 @@
         <x-tmk.form.switch id="noStock" text-off="No stock" color-off="bg-gray-100 before:line-through" text-on="No stock"
             color-on="text-white bg-lime-600" wire:model="noStock" class="w-20 h-11" />
         <x-tmk.form.switch id="noCover" text-off="Records without cover" color-off="bg-gray-100 before:line-through"
-            text-on="Records without cover" color-on="text-white bg-lime-600" class="w-44 h-11" />
-        <x-jet-button>
+            text-on="Records without cover" color-on="text-white bg-lime-600" wire:model="noCover" class="w-44 h-11" />
+        <x-jet-button wire:click="setNewRecord()">
             new record
         </x-jet-button>
     </x-tmk.section>
@@ -32,7 +32,7 @@
                     <th>Stock</th>
                     <th class="text-left">Record</th>
                     <th>
-                        <x-tmk.form.select id="perPage" class="block mt-1 w-full">
+                        <x-tmk.form.select id="perPage" class="block mt-1 w-full" wire:model="perPage">
                             <option value="5">5</option>
                             <option value="10">10</option>
                             <option value="15">15</option>
@@ -43,7 +43,7 @@
             </thead>
             <tbody>
                 @forelse($records as $record)
-                    <tr class="border-t border-gray-300">
+                    <tr class="border-t border-gray-300" wire:key="record_{{ $record->id }}">
                         <td>{{ $record->id }}</td>
                         <td>
                             <img src="{{ $record->cover['url'] }}" alt="{{ $record->title }} by {{ $record->artist }}"
@@ -58,11 +58,13 @@
                         </td>
                         <td>
                             <div class="border border-gray-300 rounded-md overflow-hidden m-2 grid grid-cols-2 h-10">
-                                <button
+                                <button wire:click="setNewRecord({{ $record->id }})"
                                     class="text-gray-400 hover:text-sky-100 hover:bg-sky-500 transition border-r border-gray-300">
                                     <x-phosphor-pencil-line-duotone class="inline-block w-5 h-5" />
                                 </button>
-                                <button class="text-gray-400 hover:text-red-100 hover:bg-red-500 transition">
+                                <button x-data=""
+                                    @click="confirm('Are you sure you want to delete this record?') ? $wire.deleteRecord({{ $record->id }}) : ''"
+                                    class="text-gray-400 hover:text-red-100 hover:bg-red-500 transition">
                                     <x-phosphor-trash-duotone class="inline-block w-5 h-5" />
                                 </button>
                             </div>
@@ -83,13 +85,66 @@
 
     <x-jet-dialog-modal id="recordModal" wire:model="showModal">
         <x-slot name="title">
-            <h2>title</h2>
+            <h2>{{ is_null($newRecord['id']) ? 'New record' : 'Edit record' }}</h2>
         </x-slot>
         <x-slot name="content">
-            content
+            @if ($errors->any())
+                <x-tmk.alert type="danger">
+                    <x-tmk.list>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </x-tmk.list>
+                </x-tmk.alert>
+            @endif
+            @if (is_null($newRecord['id']))
+                <x-jet-label for="mb_id" value="MusicBrainz id" />
+                <div class="flex flex-row gap-2 mt-2">
+                    <x-jet-input id="mb_id" type="text" placeholder="MusicBrainz ID"
+                        wire:model.defer="newRecord.mb_id" class="flex-1" />
+                    <x-jet-button wire:click="getDataFromMusicbrainzApi()" wire:loading.attr="disabled">
+                        Get Record info
+                    </x-jet-button>
+                </div>
+            @endif
+            <div class="flex flex-row gap-4 mt-4">
+                <div class="flex-1 flex-col gap-2">
+                    <p class="text-lg font-medium">{!! $newRecord['artist'] ?? '&nbsp;' !!}</p>
+                    <input type="hidden" wire:model.defer="newRecord.artist">
+                    <p class="italic">{!! $newRecord['title'] ?? '&nbsp;' !!}</p>
+                    <input type="hidden" wire:model.defer="newRecord.name">
+                    <p class="text-sm text-teal-700">{!! $newRecord['mb_id'] ? 'MusicBrainz id: ' . $newRecord['mb_id'] : '&nbsp;' !!}</p>
+                    <input type="hidden" wire:model.defer="newRecord.mb_id">
+                    <x-jet-label for="genre_id" value="Genre" class="mt-4" />
+                    <x-tmk.form.select wire:model.defer="newRecord.genre_id" id="genre_id" class="block mt-1 w-full">
+                        <option value="">Select a genre</option>
+                        @foreach ($genres as $genre)
+                            <option value="{{ $genre->id }}">{{ $genre->name }}</option>
+                        @endforeach
+                    </x-tmk.form.select>
+                    <x-jet-label for="price" value="Price" class="mt-4" />
+                    <x-jet-input id="price" type="number" step="0.01" wire:model.defer="newRecord.price"
+                        class="mt-1 block w-full" />
+                    <x-jet-label for="stock" value="Stock" class="mt-4" />
+                    <x-jet-input id="stock" type="number" wire:model.defer="newRecord.stock"
+                        class="mt-1 block w-full" />
+
+                </div>
+                <img src="{{ $newRecord['cover'] }}" alt=""
+                    class="mt-4 w-40 h-40 border border-gray-300 object-cover">
+            </div>
         </x-slot>
         <x-slot name="footer">
             <x-jet-secondary-button @click="show = false">Cancel</x-jet-secondary-button>
+            @if (is_null($newRecord['id']))
+                <x-jet-button wire:click="createRecord()" wire:loading.attr="disabled" class="ml-2">Save new record
+                </x-jet-button>
+            @else
+                <x-jet-button color="success" wire:click="updateRecord({{ $newRecord['id'] }})"
+                    wire:loading.attr="disabled" class="ml-2">Update record
+                </x-jet-button>
+            @endif
+
         </x-slot>
     </x-jet-dialog-modal>
 </div>
